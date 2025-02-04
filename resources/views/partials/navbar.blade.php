3cf42165b7da87898d4fa9536d986c09c13ab414
danibreadddev <danpammes@alu.edu.gva.es>
2025-02-04 17:19:54 +0100

    arreglo vista

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,28 @@
-<nav>
-    <ul class="d-flex gap-5">
-        @if (auth()->check())
-            <span>{{ auth()->user()->username }}</span>
-            <li><a href="{{ route('logout') }}">Logout</a></li>
-            <li><a href="{{ route('ProductList') }}">Products</a></li>
-            <li><a href="{{ route('Users.index') }}">Users</a></li>
-            <li><a href="{{ route('Users.create') }}">Create User</a></li>
-            <li><a href="{{ route('ShopCardList') }}">Mi carrito</a></li>
+<nav class="p-2 bg-primary">
+    <div class="container-fluid">
+        <div class="row d-flex align-items-center">
+            <div class="d-flex gap-5 list-unstyled">
+                @if (auth()->check())
+                    <span>{{ auth()->user()->username }}</span>
+                    <li><a href="{{ route('logout') }}">Logout</a></li>
+                    <li><a href="{{ route('ProductList') }}">Products</a></li>
+                    <li><a href="{{ route('Users.index') }}">Users</a></li>
+                    <li><a href="{{ route('Users.create') }}">Create User</a></li>
+                    <li><a href="{{ route('ShopCardList') }}">Mi carrito</a></li>
+                @else
+                    <li><a href="{{ route('index') }}">Tienda</a></li>
+                    <li><a href="{{ route('login') }}">login</a></li>
+                @endif
 
-        @else
-            <li><a href="{{ route('index') }}">Tienda</a></li>
-            <li><a href="{{ route('login') }}">login</a></li>
-        @endif
 
+            </div>
+        </div>
 
-    </ul>
+    </div>
 </nav>
+<style>
+    a {
+        color: white;
+        text-decoration: none;
+    }
+</style>
--- resources/views/shopcard/index.blade.php
+++ resources/views/shopcard/index.blade.php
@@ -1,22 +1,24 @@
 @extends('layout')
 @section('title', 'Users')
 @section('content')
-    {{ $total = 0 }}
-    <div class="container">
-        <div class="row row-cols-1">
+    <?php $total = 0; ?>
+    <div class="container-fluid">
+        <div class="row row-cols-1 g-5">
             @forelse ($shopCard as $linea)
-                <?php $total += $linea['cantidad'] * $linea['precio']?>
-                <div class="col d-flex flex-row gap-2 align-items-center">
+                <?php $total += $linea['cantidad'] * $linea['precio']; ?>
+                <div class="col d-flex flex-row gap-5 justify-content-evenly align-items-center">
 
                     <h2>producto: {{ $linea['nombre'] }}</h2>
                     <h2>Precio: {{ $linea['precio'] }}€</h2>
                     <h3>Total: {{ $linea['precio'] * $linea['cantidad'] }}€</h3>
-                    <form action="{{ route('ShopCardUpdate') }}" method="post">
+                    <form action="{{ route('ShopCardUpdate') }}" method="POST"
+                        class="d-flex flex-row gap-2 align-items-center">
                         @method('PUT')
                         @csrf
-                        <label for="cantidad">Cantidad:</label>
                         <input type="hidden" name="id" value="{{ $linea['id'] }}">
-                        <input type="text" name="cantidad" value="{{ $linea['cantidad'] }}">
+                        <label for="cantidad">Cantidad</label>
+                        <input type="text" class="form-control" name="cantidad" value="{{ $linea['cantidad'] }}">
+
                         <input type="submit" class="btn btn-warning" value="Actualizar">
                     </form>
                     <form action="{{ route('ShopCardDelete') }}" method="POST">
