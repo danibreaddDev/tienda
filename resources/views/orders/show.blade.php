f82b14eb5139a2fd750810a413d2d058fdea63e8
danibreadddev <danpammes@alu.edu.gva.es>
2025-02-07 18:34:06 +0100

    cambios

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <pre>eo{{ $orderInfo }}</pre>
     @php
-        print_r($orderInfo["order_lines"]);
+        var_dump($orderInfo["order_lines"]);
     @endphp
     <div class="px-5 container">
 
